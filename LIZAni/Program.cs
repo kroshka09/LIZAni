ec382a516ec2fc720d8d76aa8a32c25374fe9774
blackgrifen <blackgrifen@mail.ru>
2021-12-11 14:44:57 +0300

    Добавьте файлы проекта.

---
+++ LIZAni/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIZAni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Коты имба!");
+            Console.ReadKey();
+
+        }
+    }
+}
